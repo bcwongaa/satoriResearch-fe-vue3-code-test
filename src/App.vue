ab7588b9f9c845e0d6907229a91e218234c131cc
Bryan Wong <bcwongaa@gmail.com>
2024-02-29 18:19:10 +0800

    define UI structure

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Timer from './components/Timer.vue';
+import Board from './components/Board.vue';
+import Information from './components/Information.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Timer startTime="startTime" endTime="endTime"/>
+  <Board />
+  <Information />
 </template>
 
 <style scoped>
